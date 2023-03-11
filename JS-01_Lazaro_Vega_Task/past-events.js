401fc38968f54b0d5812b5eb3dac6ed8605147c7
lazaronazareno <lazaronazareno@gmail.com>
2023-03-11 12:02:54 -0300

    js-task3, checkboxes and submit events added, html refactor

--- JS-01_Lazaro_Vega_Task/past-events.js
+++ JS-01_Lazaro_Vega_Task/past-events.js
@@ -31,3 +31,95 @@ function paintCardCointainer(events) {
 }
 
 paintCardCointainer(pastEvents)
+
+function addCategories() {
+  let categories = []
+
+  pastEvents.map((item) => {
+    categories.push(item.category)
+  })
+
+  let categoriesFilter = categories.reduce((acumulador, elemento) => {
+    if (!acumulador.includes(elemento)) {
+      acumulador.push(elemento);
+    }
+    return acumulador;
+  }, []);
+
+  let template = ""
+  let checkboxContainer = document.getElementById("checkbox-container")
+
+  categoriesFilter.map((item, index) => {
+    template += `
+    <div class="form-check form-check-inline m-0">
+      <input class="form-check-input" type="checkbox" id=${`inlineCheckbox${index}`} value=${item}>
+      <label class="form-check-label" for=${`inlineCheckbox${index}`}>${item}</label>
+    </div>
+      `
+  })
+  checkboxContainer.innerHTML = template
+}
+
+addCategories()
+
+function searchByCategories(category) {
+  let results = []
+  if (category !== '')  {
+    results = pastEvents.filter((item) => {
+      return item.category.includes(category);
+    })
+  }
+
+  return results
+}
+
+let checkbox = document.getElementById("checkbox-container")
+
+let categoriesCheckbox = []
+
+checkbox.addEventListener('change', (e) => {
+  if (e.target.checked) {
+    categoriesCheckbox = categoriesCheckbox.concat(searchByCategories(e.target.value))
+  } else {
+    categoriesCheckbox = categoriesCheckbox.filter(item => !item.category.includes(e.target.value));
+  }
+  
+  return categoriesCheckbox.length === 0 ? paintCardCointainer(pastEvents) : paintCardCointainer(categoriesCheckbox)
+})
+
+// -------------------- SEARCH --------------------
+function search(word) {
+  let results = []
+  results = pastEvents.filter((item) => {
+    return item.name.toLowerCase().includes(word);
+  })  
+
+  return results
+}
+
+let searchButton = document.getElementById("search-form")
+
+searchButton.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  let results = search(e.target[0].value)
+
+  if (results.length === 0) {
+    let notResults = `
+    <div class="d-flex flex-column align-items-center gap-2 w-100">
+      <h2>No hay resultados para la busqueda : ${e.target[0].value}</h2>
+      <button class="btn btn-outline-danger" type="button" id="back-button">Volver</button>
+    </div>
+    `
+    cardContainer.innerHTML = notResults
+  
+    let backButton = document.getElementById("back-button")
+  
+    backButton.addEventListener('click', () => {
+      searchButton[0].value = ''
+      paintCardCointainer(pastEvents)
+    })
+  } else {
+    paintCardCointainer(results)
+  }
+})
